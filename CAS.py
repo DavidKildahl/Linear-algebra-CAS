2fcb8a77ab20d0f4783c6cde2206f6aee3f565bc
DavidKildahl <davidkildahl@gmail.com>
2026-04-07 20:54:10 +0200

    Gram-Schmidt tilføjet

--- CAS.py
+++ CAS.py
@@ -1,4 +1,5 @@
 from sympy import *
+from copy import deepcopy
 
 
 
@@ -59,3 +60,60 @@ A = Matrix([[0, 6, 6, 3],
 
 # Beregn udspændende vektorer for rækkerummet
 #pprint(A.rowspace())
+
+# --- Gram-Schmidt ---
+
+# Følgende kode anvender Gram-Schmidt-processen på en basis af vektorer
+
+def inner_product(u, v, coeff):
+    return sum(coeff[i] * u[i] * v[i] for i in range(len(u)))
+
+def gram_schmidt(V, coeff):
+    m, n = shape(V)
+    # Initialiser matrix. Søjler skal udgøre den ortonormale basis
+    U = zeros(m, n)
+
+    # Lav også matrix til projektionerne undervejs
+    P = zeros(m, n - 1)
+    
+    for i in range(n):
+        u = deepcopy(V[:, i]) # Undgå at ændre i oprindelige matrix
+
+        # Hvis i > 0, skal vi beregne en projektion
+        if i > 0:
+            #print(f"{i = }")
+            p = zeros(m, 1)
+            for j in range(i):
+                #print(inner_product(u, U[:, j], coeff))
+                p += inner_product(u, U[:, j], coeff) * U[:, j]
+            
+            P[:, i - 1] = p
+            u -= p
+        
+        # Normaliser
+        u /= (sqrt(inner_product(u, u, coeff)))
+
+        # Indsæt 
+        U[:, i] = u
+    
+    return U, P
+
+# Eksempel: Opgave 1, eksamen S19
+
+# V matrix med vektorer som søjler
+V = Matrix([[1, 3, 4], [1, 2, 1], [1, 1, 2]])
+
+# Koefficienter til det indre produkt
+coeff = (1, 2, 1)
+
+#pprint(V)
+U, P = gram_schmidt(V, coeff)
+#pprint(U)
+#pprint(P)
+
+
+# --- Integration ---
+
+t = symbols('t')
+# Integrate cos(t)*sin(t) on the interval [0, pi]
+#pprint(integrate(cos(t) * sin(t), (t, 0, pi)))
